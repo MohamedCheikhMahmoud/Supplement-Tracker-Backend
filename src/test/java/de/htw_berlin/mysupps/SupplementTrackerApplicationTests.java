298217eae8769aa99980679affdc5a52371559dd
Mohammad Cheikh Mahmoud <cheikhmahmoudh@gmail.com>
2026-06-14 23:57:18 +0200

    Configure test profile for PostgreSQL

--- src/test/java/de/htw_berlin/mysupps/SupplementTrackerApplicationTests.java
+++ src/test/java/de/htw_berlin/mysupps/SupplementTrackerApplicationTests.java
@@ -2,8 +2,10 @@ package de.htw_berlin.mysupps;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SupplementTrackerApplicationTests {
 
 	@Test
